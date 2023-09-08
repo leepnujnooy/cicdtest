76bf43ec537d42217b102496aecc6800c25ef6cf
Yoon Jun Pil <galaxy160102@gmail.com>
2023-09-08 11:49:44 +0900

    feat : 수정 4번째

--- src/main/java/com/example/cicdtest/controller/TestController.java
+++ src/main/java/com/example/cicdtest/controller/TestController.java
@@ -6,5 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/")
-    public String test(){}
+    public String test(){
+        return "DDD";
+    }
 }
